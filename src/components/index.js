017eb99c52a089543b75923996bbddaaa0485c13
vietthom <thomle0418@gmail.com>
2023-02-07 12:58:24 -0700

    Add logo and search bar to navbar

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,11 @@ import Feed from './Feed';
 import ChannelDetail from './ChannelDetail';
 import VideoDetail from './VideoDetail';
 import SearchFeed from './SearchFeed';
+import SearchBar from './SearchBar';
 
 export {
     Navbar,
+    SearchBar,
     Feed, 
     ChannelDetail, 
     VideoDetail, 
