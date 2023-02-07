ce711e1700d42330a0aa885f92293286ac0d8544
vietthom <thomle0418@gmail.com>
2023-02-07 08:06:55 -0700

    Established components

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Navbar from './Navbar';
+import Feed from './Feed';
+import ChannelDetail from './ChannelDetail';
+import VideoDetail from './VideoDetail';
+import SearchFeed from './SearchFeed';
+
+export {
+    Navbar,
+    Feed, 
+    ChannelDetail, 
+    VideoDetail, 
+    SearchFeed,
+}
+
